8502604dbfa51d5d39984fb30d25bf7dd40fe90d
ArbindKrPrasad <arbind.kr25801@gmail.com>
2021-02-22 13:10:47 +0530

    commit

---
+++ app/src/main/java/com/arbind/cse225/P15FragmentFirst.java
@@ -0,0 +1,21 @@
+package com.arbind.cse225;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class P15FragmentFirst extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.activity_p15_fragment_first,container, false);
+        return v;
+    }
+}
---
+++ app/src/main/java/com/arbind/cse225/P15FragmentMain.java
@@ -0,0 +1,14 @@
+package com.arbind.cse225;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class P15FragmentMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p15_fragment_main);
+    }
+}
